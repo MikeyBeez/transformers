8623240a3c3f29de9f17171e843b2b1db8b44a43
MikeyBeez <mbonsign@gmail.com>
2020-12-01 20:29:37 -0500

    fixed

--- Code/demo.py
+++ Code/demo.py
@@ -4,11 +4,23 @@ from absl import app, flags, logging
 
 import torch as th
 import pytorch_lightning as pl
-
+import numpy as np
 import nlp
 import transformers
 
 FLAGS = flags.FLAGS
+
+class IMDBSentimentClassifier(pl.lighteningModule):
+    def __init__(self):
+        super().__init__()
+    def prepare_data(self):
+        pass
+    def forward(self, batch):
+        pass
+    def training_step(self, batch, batch_idx):
+        pass
+    def train_dataloader(self):
+        pass
 
 def main(_):
     logging.info('hello')
